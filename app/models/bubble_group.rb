75500119f3835e8273473969ad44d979a88c1d62
Max <mroschke@elephantheadsoft.com>
2015-04-17 00:54:02 -0600

    -- :: Added bubble_group_status dependency relationship

--- app/models/bubble_group.rb
+++ app/models/bubble_group.rb
@@ -1,5 +1,6 @@
 class BubbleGroup < ActiveRecord::Base
   has_many :bubbles, dependent: :destroy
+  has_many :bubble_group_statuses, dependent: :destroy
 
   belongs_to :full_poset, class_name: 'Poset'
   belongs_to :forward_poset, class_name: 'Poset'
