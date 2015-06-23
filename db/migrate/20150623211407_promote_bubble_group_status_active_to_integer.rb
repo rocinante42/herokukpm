09f4b5d3335a5cfce89b36b72e205c330f21a51c
Max <mroschke@elephantheadsoft.com>
2015-06-23 16:05:27 -0600

    -- :: Added correct cast to promotion migration

--- db/migrate/20150623211407_promote_bubble_group_status_active_to_integer.rb
+++ db/migrate/20150623211407_promote_bubble_group_status_active_to_integer.rb
@@ -1,9 +1,9 @@
 class PromoteBubbleGroupStatusActiveToInteger < ActiveRecord::Migration
   def up
-    change_column :bubble_group_statuses, :active, :integer, default: BubbleGroupStatus::ACTIVE_NONE
+    change_column :bubble_group_statuses, :active, :integer, using: 'integer USING CAST(active as integer)', default: BubbleGroupStatus::ACTIVE_NONE
   end
 
   def down
-    change_column :bubble_group_statuses, :active, :boolean
+    change_column :bubble_group_statuses, :active, :boolean, using: 'boolean USING CAST(active as boolean)'
   end
 end
