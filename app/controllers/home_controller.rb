c392ca7aa36a93ae882bc9eaad4b4aaea0b8bc31
Dombrovskiy <dombrovskiy@elephantheadsoft.com>
2015-08-21 15:34:11 +0300

    #28 :: additional fix for root path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     unless user_signed_in?
   		redirect_to new_user_session_path
+    else
+      redirect_to dashboard_path
   	end
   end
 
