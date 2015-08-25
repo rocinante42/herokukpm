cd88409d128dcc0558230ea210ac75220aee81e0
Dombrovskiy <dombrovskiy@elephantheadsoft.com>
2015-08-25 17:26:12 +0300

    #35 :: Create and update Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,41 +79,6 @@ classroom_2 = Classroom.find_or_create_by({
     school_id:school_1.id
     })
 
-#/////////////////Bubble Categories///////////////
-p "Creating Categories "
-category_list = ["Cardinality","CountAll_","CountOn_","Comparison_" ]
-count = 5
-category_list.each do |category|
-   while count != 25
-      BubbleCategory.find_or_create_by({name: "#{category}#{count}"})
-      count += 5
-   end
-   count = 5
-end
-
-category_list_1 = ["Base10-Produce_","Base10-count_"]
-count_1 = 10
-category_list_1.each do |category|
-   while count_1 != 50
-      BubbleCategory.find_or_create_by({name: "#{category}#{count_1}"})
-      count_1 += 10
-   end
-   count_1 = 10
-end
-
- BubbleCategory.find_or_create_by({name: "NumberLine_10"})
- BubbleCategory.find_or_create_by({name: "NumberLine_20"})
- BubbleCategory.find_or_create_by({name: "NumberLine_70"})
- BubbleCategory.find_or_create_by({name: "NumberLine_120"})
-
-
-#/////////////////Bubble Groups///////////////
-p "Creating Bubble Groups"
-@bubble_group = BubbleGroup.find_or_create_by({
-  name: "New Count to 20",
-  description: "Counting and Cardinality"
-  })
-
 #/////////////////////GAME////////////////////
 p"Creating Games"
 
@@ -194,23 +159,68 @@ kid_5 = Kid.find_or_create_by({
   gender: 1,
   primary_language: "ENG"
   })
+
 p"Creating Posets and Bubbles"
 require 'csv'
-group_name = @bubble_group.name
-bubble_file = File.open("./db/bubbles.csv")
-full_poset_file = File.open("./db/full_poset.csv")
-forward_poset_file = File.open("./db/forward_poset.csv")
-backward_poset_file = File.open("./db/backward_poset.csv")
-bubble_arr = Bubble.create_from_csv bubble_file, {bubble_group: @bubble_group}
-bubbles = Bubble.where(id: bubble_arr.collect{ |b| b.id })
+require 'zip'
+require 'tempfile'
 
-full_poset = Poset.create_from_csv(full_poset_file, bubbles, {name: "#{group_name} -- Full Poset"})
-forward_poset = Poset.create_from_csv(forward_poset_file, bubbles, {name: "#{group_name} -- Forward Poset"})
-backward_poset = Poset.create_from_csv(backward_poset_file, bubbles, {name: "#{group_name} -- Backward Poset"})
+Zip::File.open('./db/bubble_groups.zip') do |zip_file|
+  # Handle entries one by one
+  zip_file.each do |entry|
+    next if entry.name =~ /__MACOSX/ or entry.name =~ /\.DS_Store/ or entry.file? or entry.name.eql? 'bubble_groups/'
+    puts "Extracting directory #{entry.name}"
+    bubble_group = BubbleGroup.find_or_create_by(name: entry.name.split('/').last)
 
-@bubble_group.full_poset = full_poset if full_poset
-@bubble_group.forward_poset = forward_poset if forward_poset
-@bubble_group.backward_poset = backward_poset if backward_poset
-@bubble_group.classroom_types << [type_1, type_2]
+    ##Create bubbles
+    bubble_tmp_file = Tempfile.new("bubble_tmp_file")
+    bubble_stream = zip_file.find_entry("#{entry.name}bubbles.csv").get_input_stream.read
+    bubble_tmp_file.write bubble_stream
+    bubble_tmp_file.rewind
+    bubble_arr = Bubble.create_from_csv bubble_tmp_file, {bubble_group: bubble_group}
+    bubbles = Bubble.where(id: bubble_arr.collect{ |b| b.id })
+    bubble_tmp_file.close
+    bubble_tmp_file.unlink
 
-@bubble_group.save
+    ##Create posets
+    %w(forward backward full).each do |poset_type|
+      poset_file = Tempfile.new("#{poset_type}_file")
+      poset_stream = zip_file.find_entry("#{entry.name}#{poset_type}_poset.csv").get_input_stream.read
+      poset_file.write poset_stream
+      poset_file.rewind
+      poset_data = Poset.create_from_csv(poset_file, bubbles, {name: "#{bubble_group.name} -- #{poset_type.capitalize} Poset"})
+      bubble_group.send("#{poset_type}_poset=", poset_data) if poset_data
+      poset_file.close
+      poset_file.unlink
+    end
+
+    ##Create bubble cateogries
+    category_tmp_file = Tempfile.new("category_tmp_file")
+    category_stream = zip_file.find_entry("#{entry.name}bubble_categories.csv").get_input_stream.read
+    category_tmp_file.write category_stream
+    category_tmp_file.rewind
+    BubbleCategory.create_from_csv(category_tmp_file, bubble_group: bubble_group)
+    category_tmp_file.close
+    category_tmp_file.unlink
+
+    ##Create bubble games
+    game_tmp_file = Tempfile.new("game_tmp_file")
+    game_tmp_stream = zip_file.find_entry("#{entry.name}bubble_games.csv").get_input_stream.read
+    game_tmp_file.write game_tmp_stream
+    game_tmp_file.rewind
+    BubbleGame.create_from_csv(game_tmp_file, bubble_group, {})
+    game_tmp_file.close
+    game_tmp_file.unlink
+
+    bubble_group.save
+  end
+end
+
+##adding bubble groups to classroom types(Kindergarten, First Grade)
+
+type_1.bubble_groups << BubbleGroup.where(name: %w(Base10HowMany Base10Produce CountAll CountTo20 NumberLine))
+type_2.bubble_groups << BubbleGroup.where(name: %w(Comparison CountOn))
+
+##create triggers from csv file
+Trigger.create_from_csv File.open("./db/triggers.csv")
+
