4a105f7d8e9fdecd9c09562911c85bffd2f2f3cc
Dombrovskiy <dombrovskiy@elephantheadsoft.com>
2015-09-14 15:22:57 +0300

    #94 :: ArgumentError in UsersController#dashboard

--- app/models/kid_activity.rb
+++ app/models/kid_activity.rb
@@ -2,6 +2,7 @@ class KidActivity < ActiveRecord::Base
   belongs_to :assignment
 
   def total_time
-    updated_at - created_at
+    current_progress_time = assignment.active? ? Time.now - updated_at : 0
+    current_progress_time + (updated_at - created_at)
   end
 end
