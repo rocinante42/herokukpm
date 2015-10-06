9c37febd26e628e766e9aff7cd042533489a0c92
Alex Korobeynikov <korbeynikov@elephantheadsoft.com>
2015-10-06 15:55:05 +0300

    #154 - add default order to bubble groups.

--- app/models/classroom_type.rb
+++ app/models/classroom_type.rb
@@ -1,4 +1,4 @@
 class ClassroomType < ActiveRecord::Base
  has_many :classrooms
- has_and_belongs_to_many :bubble_groups, join_table: 'bubble_groups_classroom_types'
+ has_and_belongs_to_many :bubble_groups, ->{ order id: :asc }, join_table: 'bubble_groups_classroom_types'
 end
