aa260751153eccba59bbe99668a46490a35f5d94
Alex Korobeynikov <korbeynikov@elephantheadsoft.com>
2015-10-12 15:23:36 +0300

    #131 - fix conflicts

--- app/models/classroom_type.rb
+++ app/models/classroom_type.rb
@@ -1,5 +1,4 @@
 class ClassroomType < ActiveRecord::Base
-  has_many :classrooms
-  has_and_belongs_to_many :bubble_groups
-  has_and_belongs_to_many :bubble_categories
+ has_many :classrooms
+ has_and_belongs_to_many :bubble_groups, ->{ order id: :asc }, join_table: 'bubble_groups_classroom_types'
 end
