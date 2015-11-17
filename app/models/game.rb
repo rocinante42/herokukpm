353f6d36c1d0d22554ea1f829b50cbd117e40461
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-11-17 09:26:39 +0200

    # 217 Both errors are displayed.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   validates :name, uniqueness: true
-  validates :name, length: { in: 2..50 }
+  validates :name, length: { maximum:50 }
   validates :description, length: { maximum: 255 }
   has_many :bubble_games, dependent: :destroy
   has_many :bubbles, through: :bubble_games
