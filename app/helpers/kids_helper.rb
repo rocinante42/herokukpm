52b627963f0daf39f6ec2f49052c3614a9b7d53d
Dombrovskiy <dombrovskiy@elephantheadsoft.com>
2015-11-20 17:11:27 +0200

    #233 :: removed unnecessary condition

--- app/helpers/kids_helper.rb
+++ app/helpers/kids_helper.rb
@@ -2,7 +2,7 @@ module KidsHelper
 
   def kid_avatar_for(kid:, size:)
     gender = kid.female? ? 'female' : 'male'
-    kid.female? ? "#{gender}_avatar_#{size}.png" : "#{gender}_avatar_#{size}.png"
+    "#{gender}_avatar_#{size}.png"
   end
 
 end
