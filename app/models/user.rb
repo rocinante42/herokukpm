125a2dcb5cc2df3c03576b4057475afdb2159b2d
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-12-14 13:40:27 +0200

    #268 Validation doesn't block these symbols.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 
   scope :teachers, ->{ joins(:role).where( roles: { name: "Teacher" })}
   validates_format_of :direct_phone, with: /\A\+?(\d+-)*\d+\z/, allow_blank: true
-  validates :first_name, :last_name, length: { maximum: 25 }
+  validates :first_name, :last_name, length: { maximum: 25 }, format: { with: /\A^[a-zA-Z]*$\z/, message: "only allows letters" }
   validates :direct_phone, :alternate_phone, :email, length: { maximum: 50 }
 
   validates_presence_of :classroom, if: :teacher?
