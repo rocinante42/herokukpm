43f9c9a67f930e6219a51ee2ef2138b71836bf8b
PavelNikitinskiy <paveln@elephantheadsoft.com>
2015-12-15 17:53:40 +0200

    #272 When user moves student in other classroom, the wrong classroom page is displayed.

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -101,7 +101,7 @@ class KidsController < ApplicationController
       begin
         if @kid.update(kid_params)
           url = params[:url] || @kid
-          format.html { redirect_to url, notice: 'Kid was successfully updated.' }
+          format.html { redirect_to  url, classroom: params[:kid][:classroom_id], school: params[:school], notice: 'Kid was successfully updated.' }
           format.json { render :show, status: :ok, location: @kid }
         else
           set_kid_errors format: format, action: :edit
