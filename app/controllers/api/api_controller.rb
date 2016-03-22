250993a49883c2c3f72c617862a9739910eb7f86
rocinante42 <rovertoo@gmail.com>
2016-03-22 13:26:06 -0600

    controller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -5,8 +5,17 @@ class Api::ApiController < ApplicationController
     access_token = params[:access_token]
     puts access_token
     kid = Kid.where(access_token: access_token).first
-    puts "kid: "
-    puts kid
+    puts "kid a punto de nueva funcion"
+    #new test code
+    kid = Kid.where(access_token: params[:access_token]).first
+    puts "kids where found on line 114"
+    render json: {status: :nein} and return false unless kid
+    kid.update_column(:token_expiration_time, DateTime.now + 5.minutes)
+    puts "en medio de authenticate regresando el json"
+    kids = Kid.where(classroom: kid.classroom).where.not(id: kid.id).to_a.sample(3)
+    kids << kid
+    render json: {kids: kids}
+    #test code
     render json: {status: :unathorized} and return false unless kid
 
     if kid && kid.has_expired_token?
