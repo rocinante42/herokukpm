779e449b0616f8444e2b168636c772bfcaf3b3ab
rocinante42 <rovertoo@gmail.com>
2016-03-22 04:01:02 -0600

    8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,16 @@ class ApplicationController < ActionController::Base
       headers['Access-Control-Max-Age'] = "1728000"
   end
 
+  def cors_preflight_check
+  if request.method == :options
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    render :text => '', :content_type => 'text/plain'
+  end
+end
+
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access denied!"
