23cb59a9553cc2b44cc4e3c2cef77a124ce2911d
rocinante42 <rovertoo@gmail.com>
2016-03-22 04:38:54 -0600

    next

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -5,6 +5,7 @@ class Api::ApiController < ApplicationController
     access_token = params[:access_token]
     puts access_token
     kid = Kid.where(access_token: access_token).first
+    puts "kid fracaso" unless kid
     render json: {status: :unathorized} and return false unless kid
 
     if kid && kid.has_expired_token?
