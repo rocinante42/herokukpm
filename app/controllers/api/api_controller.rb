3c08b3bd2606cd46f9dc77e455a9fc63bb758ae8
rocinante42 <rovertoo@gmail.com>
2016-03-22 13:33:07 -0600

    flag

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -7,14 +7,17 @@ class Api::ApiController < ApplicationController
     kid = Kid.where(access_token: access_token).first
     puts "kid a punto de nueva funcion"
     #new test code
-    kid = Kid.where(access_token: params[:access_token]).first
-    puts "kids where found on line 114"
-    render json: {status: :nein} and return false unless kid
-    kid.update_column(:token_expiration_time, DateTime.now + 5.minutes)
-    puts "en medio de authenticate regresando el json"
-    kids = Kid.where(classroom: kid.classroom).where.not(id: kid.id).to_a.sample(3)
-    kids << kid
-    render json: {kids: kids}
+    flag = params[:bichitos]
+    if flag
+      kid = Kid.where(access_token: params[:access_token]).first
+      puts "kids where found on line 114"
+      render json: {status: :nein} and return false unless kid
+      kid.update_column(:token_expiration_time, DateTime.now + 5.minutes)
+      puts "en medio de authenticate regresando el json"
+      kids = Kid.where(classroom: kid.classroom).where.not(id: kid.id).to_a.sample(3)
+      kids << kid
+      render json: {kids: kids}
+    end
     #test code
     render json: {status: :unathorized} and return false unless kid
 
